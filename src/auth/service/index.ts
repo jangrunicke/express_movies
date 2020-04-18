4fc33646711e999a7b60dd69149dc242e528e092
Jan Grunicke <grja1023@hs-karlsruhe.de>
2020-04-18 17:46:01 +0200

    fehlerbehung in auth/rest und .env anpassung

---
+++ src/auth/service/index.ts
@@ -0,0 +1,6 @@
+export { AuthService } from './auth.service';
+export type { LoginResult } from './auth.service';
+export * from './errors';
+export * from './role.service';
+export type { User } from './user.service';
+export { UserService } from './user.service';
